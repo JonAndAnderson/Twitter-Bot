800e957e5fa8109707feb43a4f02244f0f0493d3
JonAndAnderson <Jonandanderson3@gmail.com>
2017-12-19 20:45:46 -0500

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
- 
-module.exports = {
-consumer_key:         '4btm9HUFNtn3fNN0ucgBdDByV',
-consumer_secret:      'aSuIhT4t2nzLfE7PrxQuV9aEaYll5mgs4VyYG2ODG1joBh0jhH',
-access_token:         '899334578313285632-zsNJcMaPh1jr0WmpXhMb295KPrQJ0bz',
-access_token_secret:  'XH77h6MEYX8N17wVMgPrOFIO11Lnd0kyQB0Uk1L0WJGJW',
-}
